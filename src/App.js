82ce888083a51b2a55ef430c397df25840a0365a
Matias <matiasaramayo@hotmail.com>
2021-12-20 19:58:17 -0300

    cambios realizados para aprobrar el desafio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/header/Header.js'
+import ItemListContainer from './components/itemListContainer/ItemListContainer.js'
 import './App.css';
 
 
 
 function App() {
   return (
+    <>
     <Header />
+    <ItemListContainer />
+    </>
   );
 }
 
---
+++ src/components/itemListContainer/ItemListContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../itemListContainer/ItemListContainer.css'
+
+const itemListContainer = () => {
+    return (
+        <div class="itemListContainer">
+            <h2>En este  sitio encontrarás muchas películas y series de Anime...</h2>
+        </div>
+    )
+}
+
+export default itemListContainer
