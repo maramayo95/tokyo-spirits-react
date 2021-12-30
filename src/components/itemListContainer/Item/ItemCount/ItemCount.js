b99d934251d946138b5942a3e978ef7786806320
Matias <matiasaramayo@hotmail.com>
2021-12-29 21:16:08 -0300

    cambios en el estado del stock. Stock && counter

--- src/components/itemListContainer/Item/ItemCount/ItemCount.js
+++ src/components/itemListContainer/Item/ItemCount/ItemCount.js
@@ -3,20 +3,26 @@ import './ItemCount.css'
 
 
 const ItemCount = ({min,max}) => {
+
+    
     
     const [counter, setCounter] =  useState(min);   
+    const [stock , setStock]  = useState(9);
     
     const onAdd = () => {
         (counter < max) && setCounter(counter +1) ;
+        (stock >= 1) && setStock(stock - 1);
 
     }
 
     const onRest = () => {
        (counter > min ) &&  setCounter(counter -1) ;
+       (stock < 9) && setStock(stock + 1);
     }
 
     const resetear = () => {
         setCounter(min)
+        setStock(9)
     }
     
     
@@ -25,7 +31,7 @@ const ItemCount = ({min,max}) => {
             
             <div className="stockCantidad">
             <p>Cantidad: {counter}</p>
-            <p>Stock {max}</p>
+            <p>Stock {stock}</p>
             </div>
 
             <div className="buttons">
