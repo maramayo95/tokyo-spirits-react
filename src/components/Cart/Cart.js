b7d40dc2227e89f9edf74aa4d20a5c23d19e37b3
Matias <matiasaramayo@hotmail.com>
2022-01-19 17:25:48 -0300

    desafio terminado

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import { useCartContext } from '../../context/cartContext'
+import './Cart.css'
 
 const Cart = () => {
     const {cartList , vaciarCarrito , removeItemCarrito} = useCartContext()
 
-    //console.log(cartList)
+    console.log(cartList)
+    
     return (
         <>
-        <h1>Odio el carrito</h1>
+        <h1>Tus Compras</h1>
 
         {cartList.map(prod =>  
-            (<div key={prod.id}>
+            (<div className="contenedorCarrito" key={prod.id}>
              <li className="listadoCarrito" key={prod.id}>{prod.name} - cant: {prod.cantidad} 
              <button onClick={() => removeItemCarrito(prod.id)}>X</button>
              </li>
             </div>)
+                
               ) }
-              
         <button onClick={vaciarCarrito}>Vaciar Carrito</button>
         </>
     
